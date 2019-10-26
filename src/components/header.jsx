46ae1bb44e22f526ba2a82e99b83f552c077df31
felix <felix@mail.com>
2019-10-26 18:18:11 -0400

    fix submit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -18,7 +18,7 @@ const TodoCreator = () => {
   },[todos]);
 
   const onTodo = (e) => {
-    if(e.keyCode === 13 && !e.shiftKey){
+    if(e.keyCode === 13 && !e.shiftKey || e.type === 'submit'){
       e.preventDefault();
       const newTodo = {
         id: uuid(),
